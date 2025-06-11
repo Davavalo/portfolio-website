46aacd9a68178303cbb0cf03ea43c5682992eb34
Davavalo <86305205+Davavalo@users.noreply.github.com>
2025-06-10 22:16:55 -0500

    Website updates
    Nav bar changes

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-// Basic interactivity placeholder
-console.log("Website loaded.");
+/* Sticky Nav */
+
+window.addEventListener("scroll", function () {
+    const nav = document.querySelector(".site-nav");
+    const spacer = document.querySelector(".spacer");
+    const navHeight = nav.offsetHeight;
+  
+    if (window.scrollY > 10) {
+      nav.classList.add("sticky-nav");
+      if (spacer) spacer.style.height = navHeight + "px";
+    } else {
+      nav.classList.remove("sticky-nav");
+      if (spacer) spacer.style.height = "0px";
+    }
+  });
+  
